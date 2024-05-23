a8301fa7fccaf03085ca5847c32154cd0f18b385
iffarurrahat <iffarurrahat@gmail.com>
2024-05-23 23:35:01 +0600

    error page or 404 page part added on my website

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const myCreatedRoute = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
